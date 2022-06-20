60cd7f2c32519bf36b8d6b502d7ebe77c3c32a01
Saad Raza <saad.dr98@gmail.com>
2022-06-20 22:00:06 +0500

    trigger deployment

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import {ApolloServer} from "apollo-server";
+import {ApolloServerPluginLandingPageGraphQLPlayground} from "apollo-server-core"
 import {schema} from "./schema";
 import { context} from "./context";
 
-export const server = new ApolloServer({schema, context})
+export const server = new ApolloServer({schema, context, introspection: true, plugins: [ApolloServerPluginLandingPageGraphQLPlayground]})
 const PORT = process.env.PORT || 3000
 
 server.listen({port: PORT}).then((info) => console.log(`Server running at ${info.url}`))
